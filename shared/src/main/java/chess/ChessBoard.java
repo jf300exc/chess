840b2394b43fa6d198dc9cf6da3875e08b5e63a2
Jesse Farnsworth <jessemf@byu.edu>
2025-01-19 00:08:09 -0700

    Added private initialization methods for Pawns and Capital pieces. Implemented `resetBoard` to call initializationMethods for each both teams.

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,4 +1,5 @@
 package chess;
+import java.util.HashMap;
 
 /**
  * A chessboard that can hold and rearrange chess pieces.
@@ -7,9 +8,32 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessBoard {
+    HashMap<ChessPosition, ChessPiece> board;
+    public static final int BLACK_ROW = 8;
+    public static final int BLACK_PAWN_ROW = START_BLACK_ROW - 1;
+    public static final int WHITE_ROW = 1;
+    public static final int WHITE_PAWN_ROW = START_WHITE_ROW + 1;
 
+    public static final int ROOK_1_COL     = 1;
+    public static final int KNIGHT_1_COL   = 2;
+    public static final int BISHOP_1_COL   = 3;
+    public static final int QUEEN_COL      = 4;
+    public static final int KING_COL       = 5;
+    public static final int BISHOP_2_COL   = 6;
+    public static final int KNIGHT_2_COL   = 7;
+    public static final int ROOK_2_COL     = 8;
+
+    public static final int BOARD_SIZE     = 8;
+//    public static final int START_PAWN_1_COL     = 1;
+//    public static final int START_PAWN_2_COL     = 2;
+//    public static final int START_PAWN_3_COL     = 3;
+//    public static final int START_PAWN_4_COL     = 4;
+//    public static final int START_PAWN_5_COL     = 5;
+//    public static final int START_PAWN_6_COL     = 6;
+//    public static final int START_PAWN_7_COL     = 7;
+//    public static final int START_PAWN_8_COL     = 8;
     public ChessBoard() {
-        
+        board = new HashMap<>();
     }
 
     /**
@@ -19,7 +43,8 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        throw new RuntimeException("Not implemented");
+        board.put(position, piece);
+//        throw new RuntimeException("Not implemented");
     }
 
     /**
@@ -30,7 +55,74 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        throw new RuntimeException("Not implemented");
+        return board.get(position);
+//        throw new RuntimeException("Not implemented");
+    }
+
+    /**
+     * Adds Capital pieces (Rooks, Knights, Bishops, Queen, King) belonging
+     * to `teamColor` to their starting locations in the board.
+     *
+     * @param teamColor The team to initialize.
+     */
+    private void initializeCapitalPieces(ChessGame.TeamColor teamColor) {
+        int row;
+        if (teamColor == ChessGame.TeamColor.BLACK) {
+            row = BLACK_ROW;
+        } else {
+            row = WHITE_ROW;
+        }
+        ChessPosition position = new ChessPosition(row, ROOK_1_COL);
+        ChessPiece piece = new ChessPiece(teamColor, ChessPiece.PieceType.ROOK);
+        board.put(position, piece);
+
+        position = new ChessPosition(row, KNIGHT_1_COL);
+        piece = new ChessPiece(teamColor, ChessPiece.PieceType.KNIGHT);
+        board.put(position, piece);
+
+        position = new ChessPosition(row, BISHOP_1_COL);
+        piece = new ChessPiece(teamColor, ChessPiece.PieceType.BISHOP);
+        board.put(position, piece);
+
+        position = new ChessPosition(row, QUEEN_COL);
+        piece = new ChessPiece(teamColor, ChessPiece.PieceType.QUEEN);
+        board.put(position, piece);
+
+        position = new ChessPosition(row, KING_COL);
+        piece = new ChessPiece(teamColor, ChessPiece.PieceType.KING);
+        board.put(position, piece);
+
+        position = new ChessPosition(row, BISHOP_2_COL);
+        piece = new ChessPiece(teamColor, ChessPiece.PieceType.BISHOP);
+        board.put(position, piece);
+
+        position = new ChessPosition(row, KNIGHT_2_COL);
+        piece = new ChessPiece(teamColor, ChessPiece.PieceType.KNIGHT);
+        board.put(position, piece);
+
+        position = new ChessPosition(row, ROOK_2_COL);
+        piece = new ChessPiece(teamColor, ChessPiece.PieceType.ROOK);
+        board.put(position, piece);
+    }
+
+    /**
+     * Adds Pawn pieces belonging to `teamColor` to their starting locations
+     * in the board.
+     *
+     * @param teamColor The team to initialize.
+     */
+    private void initializePawns(ChessGame.TeamColor teamColor) {
+        int row;
+        if (teamColor == ChessGame.TeamColor.WHITE) {
+            row = WHITE_PAWN_ROW;
+        } else {
+            row = BLACK_PAWN_ROW;
+        }
+        for (int col = 1; col <= BOARD_SIZE; col++) {
+            ChessPosition position = new ChessPosition(row, col);
+            ChessPiece piece = new ChessPiece(teamColor, ChessPiece.PieceType.PAWN);
+            board.put(position, piece);
+        }
     }
 
     /**
@@ -38,6 +130,12 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+        board.clear();
+        initializeCapitalPieces(ChessGame.TeamColor.WHITE);
+        initializeCapitalPieces(ChessGame.TeamColor.BLACK);
+        initializePawns(ChessGame.TeamColor.WHITE);
+        initializePawns(ChessGame.TeamColor.BLACK);
+
+//        throw new RuntimeException("Not implemented");
     }
 }
