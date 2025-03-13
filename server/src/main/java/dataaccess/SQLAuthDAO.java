a7c7a61c5cf6551456c31dc73b8f7ede39667d2d
Jesse Farnsworth <jessemf@byu.edu>
2025-03-13 14:54:00 -0600

    Added addAuth and findAuthDataByAuthToken implements

--- server/src/main/java/dataaccess/SQLAuthDAO.java
+++ server/src/main/java/dataaccess/SQLAuthDAO.java
@@ -11,12 +11,45 @@ public class SQLAuthDAO implements AuthDAO {
 
     @Override
     public void addAuth(AuthData authData) {
+        String query = """
+                INSERT INTO auth_data(authToken,username)
+                VALUES (?, ?)
+                """;
+        try (var conn = DatabaseManager.getConnection();
+             var statement = conn.prepareStatement(query)) {
+            statement.setString(1, authData.authToken());
+            statement.setString(2, authData.username());
 
+            statement.executeUpdate();
+        } catch (DataAccessException | SQLException e) {
+            System.err.println("SQLAuthDAO: addAuth" + e.getMessage());
+        }
     }
 
     @Override
     public AuthData findAuthDataByAuthToken(String authToken) {
-        return null;
+        AuthData authData = null;
+
+        String query = """
+                SELECT * FROM auth_data WHERE authToken = ?
+                """;
+        try (var conn = DatabaseManager.getConnection();
+             var statement = conn.prepareStatement(query)) {
+            statement.setString(1, authToken);
+
+            try (var resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    String authTokenData = resultSet.getString("authToken");
+                    String username = resultSet.getString("username");
+
+                    authData = new AuthData(authTokenData, username);
+                }
+            }
+        } catch (DataAccessException | SQLException e) {
+            System.err.println("SQLAuthDAO: findAuthDataByAuthToken" + e.getMessage());
+        }
+
+        return authData;
     }
 
     @Override
@@ -47,6 +80,13 @@ public class SQLAuthDAO implements AuthDAO {
 
     @Override
     public void clear() {
+        String query = "TRUNCATE TABLE auth_data";
 
+        try (var conn = DatabaseManager.getConnection();
+             var statement = conn.prepareStatement(query)) {
+            statement.executeUpdate();
+        } catch (DataAccessException | SQLException e) {
+            System.err.println("SQLAuthDAO: clear: " + e.getMessage());
+        }
     }
 }
