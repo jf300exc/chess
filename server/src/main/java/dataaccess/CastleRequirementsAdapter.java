a0ab81d124982347211e091977d3f4468efe6284
Jesse Farnsworth <jessemf@byu.edu>
2025-03-13 18:39:50 -0600

    CastleRequirementsAdapter

--- server/src/main/java/dataaccess/CastleRequirementsAdapter.java
+++ server/src/main/java/dataaccess/CastleRequirementsAdapter.java
@@ -1,14 +1,14 @@
 package dataaccess;
 
 import java.lang.reflect.Type;
+import java.util.EnumMap;
 import java.util.Map;
 
-import chess.ChessBoard;
+import com.google.gson.*;
+
 import chess.ChessBoard.CastleType;
 import chess.ChessBoard.CastlePieceTypes;
-import chess.ChessGame;
 import chess.ChessGame.TeamColor;
-import com.google.gson.*;
 
 public class CastleRequirementsAdapter implements JsonSerializer<Map<TeamColor, Map<CastlePieceTypes, Map<CastleType, Boolean>>>>, JsonDeserializer<Map<TeamColor, Map<CastlePieceTypes, Map<CastleType, Boolean>>>>  {
 
@@ -16,14 +16,52 @@ public class CastleRequirementsAdapter implements JsonSerializer<Map<TeamColor, 
     public JsonElement serialize(Map<TeamColor, Map<CastlePieceTypes, Map<CastleType, Boolean>>> teamColorMapMap, Type type, JsonSerializationContext jsonSerializationContext) {
         JsonObject jsonObject = new JsonObject();
 
+        for (Map.Entry<TeamColor, Map<CastlePieceTypes, Map<CastleType, Boolean>>> entry : teamColorMapMap.entrySet()) {
+            JsonObject innerJson = new JsonObject();
 
+            for (Map.Entry<CastlePieceTypes, Map<CastleType, Boolean>> innerEntry : entry.getValue().entrySet()) {
+                JsonObject castleTypeMap = new JsonObject();
 
-        return null;
+                for (Map.Entry<CastleType, Boolean> castleEntry : innerEntry.getValue().entrySet()) {
+                    castleTypeMap.addProperty(castleEntry.getKey().name(), castleEntry.getValue());
+                }
+                innerJson.add(entry.getKey().name(), castleTypeMap);
+            }
+            jsonObject.add(entry.getKey().name(), innerJson);
+        }
+
+        return jsonObject;
     }
 
     @Override
     public Map<TeamColor, Map<CastlePieceTypes, Map<CastleType, Boolean>>> deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
         JsonObject jsonObject = jsonElement.getAsJsonObject();
-        return Map.of();
+        Map<TeamColor, Map<CastlePieceTypes, Map<CastleType, Boolean>>> castleRequirementsMap = new EnumMap<>(TeamColor.class);
+
+        for (Map.Entry<String, JsonElement> entry : jsonObject.entrySet()) {
+            TeamColor teamColor = TeamColor.valueOf(entry.getKey());
+            JsonObject innerJson = entry.getValue().getAsJsonObject();
+
+            Map<CastlePieceTypes, Map<CastleType, Boolean>> pieceTypeMap = new EnumMap<>(CastlePieceTypes.class);
+
+            for (Map.Entry<String, JsonElement> pieceTypeEntry : innerJson.entrySet()) {
+                CastlePieceTypes pieceTypes = CastlePieceTypes.valueOf(pieceTypeEntry.getKey());
+                JsonObject castleTypeMap = pieceTypeEntry.getValue().getAsJsonObject();
+
+                Map<CastleType, Boolean> castleTypeBooleanMap = new EnumMap<>(CastleType.class);
+
+                for (Map.Entry<String, JsonElement> castleEntry : castleTypeMap.entrySet()) {
+                    CastleType castleType = CastleType.valueOf(castleEntry.getKey());
+                    Boolean value = castleEntry.getValue().getAsBoolean();
+                    castleTypeBooleanMap.put(castleType, value);
+                }
+
+                pieceTypeMap.put(pieceTypes, castleTypeBooleanMap);
+            }
+
+            castleRequirementsMap.put(teamColor, pieceTypeMap);
+        }
+
+        return castleRequirementsMap;
     }
 }
