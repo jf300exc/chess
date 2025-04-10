c1a56b53a971da2e974e3afce3edbece81b6d93d
Jesse Farnsworth <jessemf@byu.edu>
2025-04-10 00:29:28 -0600

    Added Support of gameplay Help command

--- client/src/main/java/ui/GamePlay.java
+++ client/src/main/java/ui/GamePlay.java
@@ -1,6 +1,10 @@
 package ui;
 
+import java.util.Objects;
+import java.util.Scanner;
+
 public class GamePlay implements WebSocketListener {
+    private final Scanner scanner = new Scanner(System.in);
     private WebSocketClient ws;
     private UserType userType;
 
@@ -37,7 +41,79 @@ public class GamePlay implements WebSocketListener {
         ws.closeClient();
     }
 
-    private void runGamePlayUI() throws Exception {
+    private String userTypePromptString() {
+        if (userType == UserType.PLAYER) {
+            return "[PLAYING]";
+        }
+        return "[OBSERVING]";
+    }
 
+    private String getUserInput(String prompt) {
+        System.out.print(Objects.requireNonNullElseGet(prompt, () -> userTypePromptString() + " >>> "));
+        return scanner.nextLine().trim();
+    }
+
+    private void runGamePlayUI() throws Exception {
+        for (;;) {
+            String userInput = getUserInput(null);
+            if (!matchGamePlayCommand(userInput)) {
+                break;
+            }
+        }
+    }
+
+    private boolean matchGamePlayCommand(String command) throws Exception {
+        if (command.isBlank()) {
+            return true;
+        }
+        switch (command) {
+            case "Help" -> displayGamePlayHelp();
+            case "Redraw Chess Board" -> redrawBoard();
+            case "Leave" -> {
+                leaveGame();
+                return false;
+            }
+            case "Make Move" -> gamePlayMakeMove();
+            case "Resign" -> resignGame();
+            case "Highlight", "Highlight Legal Moves" -> highlightMoves();
+            default -> matchArbitraryCommand(command);
+        }
+        return true;
+    }
+
+    private void matchArbitraryCommand(String command) {
+        System.out.println("Unknown command.");
+    }
+
+    private static void displayGamePlayHelp() {
+        String helpMessage = """
+                Available commands for GamePlay UI:
+                    Help                    Displays this help message.
+                    Redraw Chess Board      Redraws the chess board.
+                    Leave                   Leave the game.
+                    Make Move               Make a move. (Ex. a2a4)
+                    Resign                  Forfeit the game.
+                    Highlight Legal Moves   Highlights available moves.""";
+        System.out.println(helpMessage);
+    }
+
+    private void redrawBoard() {
+        throw new RuntimeException("Not implemented");
+    }
+
+    private void leaveGame() {
+        throw new RuntimeException("Not implemented");
+    }
+
+    private void gamePlayMakeMove() {
+        throw new RuntimeException("Not implemented");
+    }
+
+    private void resignGame() {
+        throw new RuntimeException("Not implemented");
+    }
+
+    private void highlightMoves() {
+        throw new RuntimeException("Not implemented");
     }
 }
