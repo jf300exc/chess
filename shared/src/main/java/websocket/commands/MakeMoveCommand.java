7acf780084d899a24e5b509e31237556b7654cc7
Jesse Farnsworth <jessemf@byu.edu>
2025-04-09 20:22:16 -0600

    Added hashCode and equals methods to MakeMoveCommand

--- shared/src/main/java/websocket/commands/MakeMoveCommand.java
+++ shared/src/main/java/websocket/commands/MakeMoveCommand.java
@@ -2,6 +2,8 @@ package websocket.commands;
 
 import chess.ChessMove;
 
+import java.util.Objects;
+
 public class MakeMoveCommand extends UserGameCommand {
     private final ChessMove move;
 
@@ -12,5 +14,22 @@ public class MakeMoveCommand extends UserGameCommand {
 
     public ChessMove getMove() {
         return move;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        if (!super.equals(o)) {
+            return false;
+        }
+        MakeMoveCommand that = (MakeMoveCommand) o;
+        return Objects.equals(move, that.move);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), move);
     }
 }
