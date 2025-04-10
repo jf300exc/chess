742297bd51849bea31f93ba7a22aa2583c343c0c
Jesse Farnsworth <jessemf@byu.edu>
2025-04-09 20:22:52 -0600

    Added ErrorMessage

---
+++ shared/src/main/java/websocket/messages/ErrorMessage.java
@@ -0,0 +1,33 @@
+package websocket.messages;
+
+import java.util.Objects;
+
+public class ErrorMessage extends ServerMessage {
+    String errorMessage;
+
+    public ErrorMessage(ServerMessageType type, String errorMessage) {
+        super(type);
+        this.errorMessage = errorMessage;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        if (!super.equals(o)) {
+            return false;
+        }
+        ErrorMessage that = (ErrorMessage) o;
+        return Objects.equals(errorMessage, that.errorMessage);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), errorMessage);
+    }
+}
